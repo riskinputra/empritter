dc08cf5f0898dd9cfdf2bb5342517e616c4815fb
riskinputra <riski.n.putra@gmail.com>
2018-01-18 13:31:00 +0700

    checkpoint 1

--- server/app.js
+++ server/app.js
@@ -4,18 +4,29 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors')
 
 const index = require('./routes/index');
 const users = require('./routes/users');
 
 const app = express();
-
+require('dotenv').config()
+const db = mongoose.connection;
+// mongoose.connect('mongodb://localhost:27017/mpritter')
+mongoose.connect(`mongodb://admin:${process.env.PASSWORD}@cluster0-shard-00-00-4ut9u.mongodb.net:27017,cluster0-shard-00-01-4ut9u.mongodb.net:27017,cluster0-shard-00-02-4ut9u.mongodb.net:27017/mpritter?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`)
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('we\'re connected to database!')
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
